412bcb7622476b7f2061f8e1dd98e03add8d6e95
Ssmurf8571 <mastervkirilla@gmail.com>
2021-03-11 20:31:00 +0300

    Added register and login form

--- js/common.js
+++ js/common.js
@@ -1,49 +1,10 @@
-$(function() {
-
-	//SVG Fallback
-	if(!Modernizr.svg) {
-		$("img[src*='svg']").attr("src", function() {
-			return $(this).attr("src").replace(".svg", ".png");
-		});
-	};
-
-	//E-mail Ajax Send
-	//Documentation & Example: https://github.com/agragregra/uniMail
-	$("form").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			alert("Thank you!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
-
-	//Chrome Smooth Scroll
-	try {
-		$.browserSelector();
-		if($("html").hasClass("chrome")) {
-			$.smoothScroll();
-		}
-	} catch(err) {
-
-	};
-
-	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
-	
-});
 
 $(window).on('load', function() {
 
 	$(".loader_inner").fadeOut();
 	$(".loader").delay(400).fadeOut("slow");
 });
+
 
 $( ".app__nav__language__current" ).click(function() {
   var dropdown_btn_lang = document.getElementById('app__nav__language__dropdown');
@@ -53,4 +14,48 @@ $( ".app__nav__language__current" ).click(function() {
 $( ".app__nav__page__nightmode" ).click(function() {
   var body_dark__mode = document.body;
   body_dark__mode.classList.toggle("dark-mode");
+});
+
+
+$( "#login_btn" ).click(function() {
+
+	document.getElementById("id01").style.display = "block";
+
+
+	var modal = document.getElementById('id01');
+
+	window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+	}
+});
+
+
+$( ".close" ).click(function() {
+
+	document.getElementById("id01").style.display = "none";
+	document.getElementById("id02").style.display = "none";
+
+});
+
+$(".cancelbtn").click(function() {
+    document.getElementById("id01").style.display = "none";
+	document.getElementById("id02").style.display = "none";
+});
+
+
+$( ".register_btn" ).click(function() {
+
+	document.getElementById("id01").style.display = "none";
+	document.getElementById("id02").style.display = "block";
+
+
+	  var modal = document.getElementById('id02');
+  
+	  window.onclick = function(event) {
+		  if (event.target == modal) {
+			  modal.style.display = "none";
+		  }
+	  }
 });
