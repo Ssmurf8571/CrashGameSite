4a33d5f2a0b5c43e03f5206d743e762f062d3eda
Ssmurf8571 <mastervkirilla@gmail.com>
2021-03-13 12:56:24 +0300

    Update file

--- php/reg.php
+++ php/reg.php
@@ -24,7 +24,7 @@
   $result = mysqli_query($db,"SELECT id FROM users WHERE login='$login'");
   $myrow = mysqli_fetch_array($result);
   if (!empty($myrow['id'])) {
-    exit ("Извините, введённый вами логин уже зарегистрирован. Введите другой логин.");
+    exit ("<center><h1>Извините, введённый вами логин уже зарегистрирован. Введите другой логин.</h1></center>");
   }
 
   $result2 = mysqli_query ($db, "INSERT INTO users (login,password,email) VALUES('$login','$password','$email')");
