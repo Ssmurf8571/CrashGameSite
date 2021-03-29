3ce61cdc10629560d9d3eda13fa891d90b99a3df
Ssmurf8571 <mastervkirilla@gmail.com>
2021-03-29 19:04:03 +0300

    Update file

--- en/send_msg.php
+++ en/send_msg.php
@@ -3,11 +3,10 @@
 
     date_default_timezone_set('ALL');
 
-    $user = $_GET['login'];
-    $msg = $_GET['msg'];
+    $user = $_POST['login'];
+    $msg = $_POST['msg'];
     $data = date('G:i');
 
     $result = mysqli_query($db, "INSERT INTO message (user,message,date) VALUES('$user','$msg','$data')");
 
-    header('Location: /CrashGameSite/en/index.php');
 ?>
