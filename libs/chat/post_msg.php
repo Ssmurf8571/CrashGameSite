feb173a5065d3bf7a7cf2257b02dff0167e9e097
Ssmurf8571 <mastervkirilla@gmail.com>
2021-04-06 10:21:09 +0300

    Deleted comments

--- libs/chat/post_msg.php
+++ libs/chat/post_msg.php
@@ -2,20 +2,6 @@
 
     require __DIR__ . '/../authorization/db.php';
 
-    // $result = mysqli_query($db,"SELECT user, date, message FROM message");
-
-    // $rows = mysqli_num_rows($result);
-    
-    // for ($i = 0 ; $i < $rows ; ++$i)
-    // {
-    //     $row = mysqli_fetch_row($result);
-    //         $j=0; echo '<span class="username">'.$row[$j].'</span>';
-    //         $j=1; echo '<span class="date">'.$row[$j].'</span>';
-    //         $j=2; echo '<span class="umessage">'.$row[$j].'</span>';
-    //     echo "<br>";
-    // }
-
-
     $result = mysqli_query($db,"SELECT user, date, message FROM message");
     while ($row = $result->fetch_assoc()) {
         echo '<span class="username">'.$row['user'].'</span>';
