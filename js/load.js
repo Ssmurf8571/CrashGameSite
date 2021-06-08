414d9fba6b0fbf81c7e4195a0e9087cf78b741f0
Ssmurf8571 <mastervkirilla@gmail.com>
2021-06-08 18:08:21 +0300

    Updated script code

--- js/load.js
+++ js/load.js
@@ -1,5 +1,7 @@
-$(window).on('load', function() {
+let classes = ['#chatwindow', '#nvuti', '#wheel', '#login__form', '#register_form', '#promo_box', '#formDonate'];	
 
-	$(".loader_inner").fadeOut();
-	$(".loader").delay(400).fadeOut("slow");
+$.each(classes, function(i) {
+	let pages = ['../libs/chat/post_msg.php', '../libs/games/dice/index.html', '../libs/games/wheel/index.html', '../login.html', '../register.html', '../libs/cash/promo/promo.html', '../libs/cash/donate/index.html'];
+
+	$(classes[i]).load(pages[i]);
 });
