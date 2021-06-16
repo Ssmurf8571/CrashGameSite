36c17f712e7bc091c9a8b181803ff8d8f4a09458
Ssmurf8571 <mastervkirilla@gmail.com>
2021-06-16 18:49:38 +0300

    Fixed chat

--- libs/chat/post_msg.php
+++ libs/chat/post_msg.php
@@ -4,10 +4,13 @@
 
     $result = mysqli_query($db,"SELECT user, date, message FROM message");
     while ($row = $result->fetch_assoc()) {
+
         echo '<span class="username">'.$row['user'].'</span>';
         echo '<span class="date">'.$row['date'].'</span>';
-        echo '<span class="umessage">'.$row['message'].'</span>';
+
+        echo '<span class="umessage"><xmp>'.$row['message'].'</xmp></span>';
         echo '<br>';
+
     }
     
     mysqli_free_result($result);
